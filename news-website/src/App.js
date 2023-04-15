05e382813e774d4e7dcd3a66283b22e783d0212f
Charity <charity.grey1@gmail.com>
2023-04-15 09:17:56 -0700

    Create React App

---
+++ news-website/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <p>
+          Edit and save to reload.
+          what what what OH MY GOODENSS IT"S IN REAL TIME YAY 
+        </p>
+    </div>
+  );
+}
+
+export default App;
